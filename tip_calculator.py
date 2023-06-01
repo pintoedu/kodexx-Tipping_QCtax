e032672b975bad5c620023be0705a703ace900fe
kodexx <65950405+pintoedu@users.noreply.github.com>
2023-06-01 07:57:09 -0400

    Update tip_calculator.py

--- tip_calculator.py
+++ tip_calculator.py
@@ -11,7 +11,6 @@ while True:
         print("Please enter a valid number.")
         
         
-   
 # Created variable to get a float number as we are fetching the cost of the meal (without taxes yet)
 while True:
     cost_meal = input("What is the cost of all meals before taxes?: > ")
